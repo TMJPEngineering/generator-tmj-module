d6e380f8b15c6a2e86f2678add6ba373e824ef82
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-03-03 17:04:53 +0800

    rename changes

--- make/index.js
+++ make/index.js
@@ -1,17 +1,31 @@
 'use strict';
 
-var generator = require('yeoman-generator');
+var generator = require('yeoman-generator'),
+    error = require('./../error'),
+    chalk = require('chalk');
 
 module.exports = generator.extend({
     initializing: function () {
+        if (this.args.length === 0) {
+            this.log(chalk.yellow('\nUsage:\n') + '  yo tmj-module:make [module]\n\n' + chalk.yellow('Error:\n') + chalk.red('  - name of module is required'));
+            process.exit(1);
+        }
+
         this.argument('name', { type: String, required: true });
 
-        var name = this.arguments.shift();
+        var module = this.arguments.shift();
 
         var data = {
-            arguments: [name, name]
+            arguments: [module, module]
         };
 
+        this.composeWith('tmj-module:make-angular-config', data);
+        this.composeWith('tmj-module:make-angular-controller', data);
+        this.composeWith('tmj-module:make-angular-directive', data);
+        this.composeWith('tmj-module:make-angular-factory', data);
+        this.composeWith('tmj-module:make-angular-module', data);
+        this.composeWith('tmj-module:make-angular-provider', data);
+        this.composeWith('tmj-module:make-angular-service', data);
         this.composeWith('tmj-module:make-controller', data);
         this.composeWith('tmj-module:make-middleware', data);
         this.composeWith('tmj-module:make-model', data);
@@ -19,15 +33,7 @@ module.exports = generator.extend({
         this.composeWith('tmj-module:make-schema', data);
         this.composeWith('tmj-module:make-test', data);
         this.composeWith('tmj-module:make-view', {
-            arguments: ['index', name]
+            arguments: ['index', module]
         });
-
-        this.composeWith('tmj-module:make-client-config', data);
-        this.composeWith('tmj-module:make-client-controller', data);
-        this.composeWith('tmj-module:make-client-directive', data);
-        this.composeWith('tmj-module:make-client-factory', data);
-        this.composeWith('tmj-module:make-client-module', data);
-        this.composeWith('tmj-module:make-client-provider', data);
-        this.composeWith('tmj-module:make-client-service', data);
     }
 });
