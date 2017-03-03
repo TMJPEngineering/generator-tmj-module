72200e10ce118d4de6beb4e98038544923979e0e
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-03-03 10:33:36 +0800

    add make test

--- make-test/templates/test.js
+++ make-test/templates/test.js
@@ -1,5 +1,10 @@
 'use strict'
 
+/**
+ * @author TMJP Engineering Team
+ * @copyright <%= year %>
+ */
+
 describe('<%= name %>', function () {
     it('should return true', function () {
         expect(true).toBeTruthy();
