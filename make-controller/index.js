bfc74c89b9d85375942b32b17b1d10710a64b4eb
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-03-03 10:15:59 +0800

    add make controller and update configuration

--- make-controller/index.js
+++ make-controller/index.js
@@ -1,10 +1,26 @@
 'use strict';
 
-var generator = require('yeoman-generator');
+var generator = require('yeoman-generator'),
+    error = require('./../error'),
+    constants = require('./../constants');
 
 module.exports = generator.extend({
-    writing: function () {
-        this.log('Controller called');
-        this.log(this.arguments);
+    initializing: function () {
+        error(this, ['controller', 'module'], 'controller');
+        this.argument('name', { type: String, required: true });
+        this.argument('module', { type: String, required: true });
+    },
+    execute: function () {
+        var data = {
+            name: this.args.shift(),
+            module: this.args.shift(),
+            year: new Date().getFullYear()
+        };
+
+        this.fs.copyTpl(
+            this.templatePath('server/controller.js'),
+            this.destinationPath(constants.module.path + '/' + data.module + '/server/' + data.name + '.controller.js'),
+            data
+        );
     }
 });
---
+++ make-controller/templates/server/controller.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @author TMJP Engineering Team
+ * @copyright <%= year %>
+ */
+
+var path = require('path');
+
+module.exports = function (method) {
+    var methods = {
+        index: index
+    };
+
+    return methods[method]();
+
+    function index() {
+        return function (req, res, next) {
+            if (req.route.methods.get) {
+                res.sendFile(path.join(__dirname, '../../../resouces/views/<%= module %>', 'index.html'));
+            }
+        }
+    }
+};
