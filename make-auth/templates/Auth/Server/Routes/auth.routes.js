a0c52b38f7fbfac6a9c23c60fcb24c46746c74ec
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-03-13 13:04:22 +0800

    make auth

---
+++ make-auth/templates/Auth/Server/Routes/auth.routes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var path = require('path'),
+    root = path.dirname(require.main.filename),
+    route = require(root + '/vendor/router');
+
+module.exports = function (app) {
+    route.setModule('Auth');
+
+    route.get('/home', 'AuthController@index', ['Auth::protect']);
+
+    route.get('/login', 'AuthController@login');
+    route.get('/register', 'AuthController@register');
+
+    route.post('/login', 'AuthController@login');
+    route.post('/register', 'AuthController@register');
+};
---
+++ make-auth/templates/Welcome/Client/Config/welcome.config.js
@@ -0,0 +1,30 @@
+/**
+ * @author TMJP Web Development Team
+ * @copyright 2016
+ */
+
+(function () {
+    'use strict';
+
+    angular.module('welcome')
+        .config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+
+    function config($stateProvider, $urlRouterProvider, $locationProvider) {
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('welcome', {
+                url: '/',
+                views: {
+                    'nav': { templateUrl: 'views/layouts/nav.html' },
+                    'content': {
+                        templateUrl: 'views/welcome/index.html',
+                        controller: 'WelcomeController',
+                        controllerAs: 'wc',
+                    }
+                }
+            });
+        $locationProvider.html5Mode(true);
+    }
+})();
